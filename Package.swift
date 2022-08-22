56536a6fc63e4e4de1bfce077b818a7ed9d14910
Tony <vungoc.dang@yum.com>
2022-08-22 09:34:12 +0700

    init code

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:4.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "libPhoneNumber",
+    products: [
+        .library(
+            name: "libPhoneNumber",
+            targets: ["libPhoneNumber"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libPhoneNumber",
+            path: "libPhoneNumber",
+            exclude: ["GeneratePhoneNumberHeader.sh", "NBPhoneNumberMetadata.plist", "Info.plist"],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ]
+        ),
+        .testTarget(
+            name: "libPhoneNumberTests",
+            dependencies: ["libPhoneNumber"],
+            path: "libPhoneNumberTests",
+            sources: [
+                "NBAsYouTypeFormatterTest.m",
+                "NBPhoneNumberParsingPerfTest.m",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.h",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.m",
+                "NBPhoneNumberUtilTest.m",
+                "NBShortNumberInfoTest.m"
+            ]
+        )
+    ]
+)
